29f9f1f49446e872610dfe9448235b5e36f01156
jt <jt2594838@163.com>
2018-05-14 11:53:32 +0800

    add arg.repeat

--- src/scripts2/test_map_validate.sh
+++ src/scripts2/test_map_validate.sh
@@ -15,6 +15,7 @@ description="l5000"
 threshold=0.9
 apply_method="apply_loss4D"
 gpu_no="0"
+repeat=10
 
 python -u ${python_file} \
         -batch_size ${batch_size} \
@@ -31,4 +32,5 @@ python -u ${python_file} \
         ${preprocess}   \
         -threshold ${threshold} \
         -apply_method ${apply_method} \
-        -gpu_no ${gpu_no}
+        -gpu_no ${gpu_no}   \
+        -repeat ${repeat}
