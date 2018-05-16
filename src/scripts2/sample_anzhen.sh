022c17d10a8c73831bedc1bd806c9917283e9b31
jt <jt2594838@163.com>
2018-05-16 12:09:58 +0800

    add sample_anzhen

---
+++ src/scripts2/sample_anzhen.sh
@@ -0,0 +1,9 @@
+python_file='process/sample_anzhen.py'
+input=''
+sample_rate=0.5
+output=''
+
+python -u ${python_file} \
+        -input ${input} \
+        -sample_rate ${sample_rate}  \
+        -output ${output} 
