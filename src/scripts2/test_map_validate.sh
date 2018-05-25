cd9e92973115cec4a5b30d2f8ac8c9fcc8b7054f
jt2594838@163.com <jt2594838@163.com>
2018-05-25 22:28:09 +0800

    for merge

--- src/scripts2/test_map_validate.sh
+++ src/scripts2/test_map_validate.sh
@@ -1,18 +1,18 @@
 python_file='train/test_map_validate.py'
-batch_size=50
+batch_size=1
 classes=3
 dataset="anzhen"
 # pretrained="-pretrained 1"
 model="ResNet101"
 model_path="/home/jiangtian/code/gp/res_anzhen/original_model/ResNet101_anzhen_3_200_98.35051569987819.pkl"
-map_dir="/home/jiangtian/code/gp/res_anzhen/val_map/Deeplab_anzhen_zero_sp50_quality_validate_l4370.h5"
+map_dir="/home/jiangtian/code/gp/res_anzhen/val_map/ConvDeconvV2_anzhen_one_sp100_quality_validate_l4370.h5"
 # preprocess="-preprocess 1"
 threshold="0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95,0.99,1.0"
 apply_method="apply_loss4D"
-gpu_no="0"
-output="/home/jiangtian/code/gp/res_anzhen/val_rst/Deeplab_anzhen_zero_sp50_quality_validate_l4370.h5"
+gpu_no="1"
+output="/home/jiangtian/code/gp/res_anzhen/val_rst_recall/ConvDeconvV2_anzhen_one_sp100_quality_validate_l4370.h5"
 repeat=1
-criterion="auc_roc"
+criterion="prec"
 
 python -u ${python_file} \
         -batch_size ${batch_size} \
