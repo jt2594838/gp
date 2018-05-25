80524ec2bf81c96ff3db06c442b05e98b72724b7
jt2594838@163.com <jt2594838@163.com>
2018-05-25 18:40:28 +0800

    for merge

--- src/scripts2/test_map_validate.sh
+++ src/scripts2/test_map_validate.sh
@@ -1,19 +1,17 @@
 python_file='train/test_map_validate.py'
 batch_size=50
-epoch=50
 classes=3
-val_dir="/home/jiangtian/code/gp/data/anzhen/merged2"
 dataset="anzhen"
 # pretrained="-pretrained 1"
 model="ResNet101"
 model_path="/home/jiangtian/code/gp/res_anzhen/original_model/ResNet101_anzhen_3_200_98.35051569987819.pkl"
-map_dir="/home/jiangtian/code/gp/res_anzhen/val_map/ConvDeconvV2_anzhen_zero_rect_greed_quality_train_l4370.h5"
+map_dir="/home/jiangtian/code/gp/res_anzhen/val_map/Deeplab_anzhen_zero_sp50_quality_validate_l4370.h5"
 # preprocess="-preprocess 1"
-threshold="0.5,0.6,0.7,0.8,0.9,0.95,0.99,1.0"
+threshold="0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95,0.99,1.0"
 apply_method="apply_loss4D"
-gpu_no="5"
-output="/home/jiangtian/code/gp/res_anzhen/val_rst/ConvDeconv_zero_rect_quality.rst"
-repeat=10
+gpu_no="0"
+output="/home/jiangtian/code/gp/res_anzhen/val_rst/Deeplab_anzhen_zero_sp50_quality_validate_l4370.h5"
+repeat=1
 
 python -u ${python_file} \
         -batch_size ${batch_size} \
@@ -23,9 +21,9 @@ python -u ${python_file} \
         -model ${model} \
         -model_path ${model_path} \
         -map_dir ${map_dir} \
-        -val_dir ${val_dir} \
         ${preprocess}   \
         -threshold ${threshold} \
         -apply_method ${apply_method} \
         -gpu_no ${gpu_no}   \
-        -repeat ${repeat}
+        -repeat ${repeat}   \
+	-output ${output}
